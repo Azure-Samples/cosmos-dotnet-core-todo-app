0e76996a371c96ae14b8624a3a0c1d99adc388c3
Matias Quaranta <maquaran@microsoft.com>
2020-06-19 10:05:38 -0700

    removing fluent

--- src/Startup.cs
+++ src/Startup.cs
@@ -63,10 +63,7 @@
             string containerName = configurationSection.GetSection("ContainerName").Value;
             string account = configurationSection.GetSection("Account").Value;
             string key = configurationSection.GetSection("Key").Value;
-            Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder clientBuilder = new Microsoft.Azure.Cosmos.Fluent.CosmosClientBuilder(account, key);
-            Microsoft.Azure.Cosmos.CosmosClient client = clientBuilder
-                                .WithConnectionModeDirect()
-                                .Build();
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
             CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
             Microsoft.Azure.Cosmos.DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
             await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
