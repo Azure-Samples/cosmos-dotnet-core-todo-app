3dcfb36aeae7b2af51b6501865dd7e64253c9a94
hyesam2 <85204502+hyesam2@users.noreply.github.com>
2022-07-12 18:53:34 +0900

    Update Item.cs

--- src/Models/Item.cs
+++ src/Models/Item.cs
@@ -7,15 +7,6 @@
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
         
-        [JsonProperty(PropertyName = "messageId")]
-        public string messageId { get; set; }
-        
-        [JsonProperty(PropertyName = "deviceId")]
-        public string deviceId { get; set; }
-
-        [JsonProperty(PropertyName = "humidity")]
-        public string humidity { get; set; }
-
         [JsonProperty(PropertyName = "name")]
         public string Name { get; set; }
 
